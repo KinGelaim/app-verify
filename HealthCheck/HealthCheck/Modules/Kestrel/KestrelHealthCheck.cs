1ea22eb11477ebf815df7c76f799a86f00434b59
KinGelaim <89123232156@mail.ru>
2024-06-08 10:24:47 +0500

    The system check has been updated

--- HealthCheck/HealthCheck/Modules/Kestrel/KestrelHealthCheck.cs
+++ HealthCheck/HealthCheck/Modules/Kestrel/KestrelHealthCheck.cs
@@ -1,34 +1,33 @@
-﻿//using Microsoft.AspNetCore.Builder;
-//using Microsoft.Extensions.Logging;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 
 
-//namespace HealthCheck.Modules.Kestrel;
+namespace HealthCheck.Modules.Kestrel;
 
-//public partial class KestrelHealthCheck
-//{
-//    private readonly WebApplication _app;
+public partial class KestrelHealthCheck
+{
+    private readonly WebApplication _app;
 
-//    private KestrelHealthCheck(WebApplication app)
-//    {
-//        _app = app;
-//    }
+    private KestrelHealthCheck(WebApplication app)
+    {
+        _app = app;
+    }
 
-//    /// <summary>
-//    /// Создание построителя для модуля HealthCheck
-//    /// </summary>
-//    public static KestrelHealthCheckBuilder GetBuilder() => new();
+    /// <summary>
+    /// Создание построителя для модуля HealthCheck
+    /// </summary>
+    public static KestrelHealthCheckBuilder GetBuilder() => new();
 
-//    /// <summary>
-//    /// Запуск веб-сервера для ожидания обращений к healthCheck
-//    /// </summary>
-//    /// <param name="logger">Логгер</param>
-//    public async Task RunAsync(ILogger? logger = null)
-//    {
-//        _ = _app.RunAsync()
-//            .ContinueWith(t =>
-//            {
-//                logger?.LogError(t.Exception, $"{nameof(KestrelHealthCheck)}.{nameof(RunAsync)}: error during starting healthCheck.");
-//            }, TaskContinuationOptions.OnlyOnFaulted);
-//    }
-//}
+    /// <summary>
+    /// Запуск веб-сервера для ожидания обращений к healthCheck
+    /// </summary>
+    /// <param name="logger">Логгер</param>
+    public async Task RunAsync(ILogger? logger = null)
+    {
+        _ = _app.RunAsync()
+            .ContinueWith(t =>
+            {
+                logger?.LogError(t.Exception, $"{nameof(KestrelHealthCheck)}.{nameof(RunAsync)}: error during starting healthCheck.");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+    }
+}
